1b839ec9368c692a018261f8ee6a7306dceb92b2
user <user@users-MacBook-Pro.local>
2019-05-07 13:02:21 +0300

    addingStringBuilder

--- src/main/java/com/dp/test/CsvWriter.java
+++ src/main/java/com/dp/test/CsvWriter.java
@@ -7,31 +7,69 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import generated.Invoice;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class CsvWriter {
 
-    public static void write(Invoice invoice) throws IOException {
-        CsvMapper mapper = new CsvMapper();
-        CsvSchema schema =   mapper
-                .typedSchemaFor(Invoice.class)
-                .withHeader();
 
-        String csv = mapper.writer(schema).writeValueAsString(invoice);
-        File file = new File("/Users/user/Desktop/invoice.csv");
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-        try (PrintWriter out = new PrintWriter(file.getPath())) {
-            out.write(csv);
-        } catch (FileNotFoundException e) {
-           e.printStackTrace();
-        }
+// Metoda 1 de lucru : -------------------------- Descriere citire CSV prin CSV Mapper----------------------------------
 
-    }
+//    public static void write(Invoice invoice) throws IOException {
+//        CsvMapper mapper = new CsvMapper();
+//        CsvSchema schema =   mapper
+//                .typedSchemaFor(Invoice.class)
+//                .withHeader();
+//
+//        String csv = mapper.writer(schema).writeValueAsString(invoice);
+//        File file = new File("/Users/user/Desktop/invoice.csv");
+//        if (!file.exists()) {
+//            file.createNewFile();
+//        }
+//        try (PrintWriter out = new PrintWriter(file.getPath())) {
+//            out.write(csv);
+//        } catch (FileNotFoundException e) {
+//           e.printStackTrace();
+//        }
+//
+//    }
 
     // o noua functie ce foloseste StringBuilder in loc de csv Mapper
+
+
+// Metoda 2 de lucru : -------------------------- Descriere citire CSV prin StringBuilder ----------------------------------
+
+
+
+    private static final String CSV_SEPARATOR = ",";
+    public static void write(Invoice product)
+    {
+        try
+        {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/Users/user/Documents/projects/products.csv"), "UTF-8"));
+
+
+                StringBuffer oneLine = new StringBuffer();
+                oneLine.append("issue,nr,total");
+                oneLine.append("\n");
+                oneLine.append(product.getNr() <=0 ? "" : product.getNr());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(product.getIssue()<=0? "" : product.getIssue());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(product.getTotal() < 0 ? "" : product.getTotal());
+//                oneLine.append(product.isVatApplicable() ? "Yes" : "No");
+                bw.write(oneLine.toString());
+                bw.newLine();
+
+            bw.flush();
+            bw.close();
+        }
+        catch (UnsupportedEncodingException e) {}
+        catch (FileNotFoundException e){}
+        catch (IOException e){}
+    }
+
+
+
+
+
 }
